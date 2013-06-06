78967a59ecb7a704960084dfa381fb5647fcc403
Johannes Ernst <Johannes.Ernst@InfoGrid.org>
2013-06-06 23:09:51 +0000

    support account status of closed

--- ig-lid/modules/org.infogrid.lid/src/org/infogrid/lid/account/LidAccount.java
+++ ig-lid/modules/org.infogrid.lid/src/org/infogrid/lid/account/LidAccount.java
@@ -8,7 +8,7 @@
 // 
 // For more information about InfoGrid go to http://infogrid.org/
 //
-// Copyright 1998-2012 by R-Objects Inc. dba NetMesh Inc., Johannes Ernst
+// Copyright 1998-2013 by R-Objects Inc. dba NetMesh Inc., Johannes Ernst
 // All rights reserved.
 //
 
@@ -121,6 +121,7 @@ public interface LidAccount
     {
         APPLIEDFOR,
         ACTIVE,
+        CLOSED,
         DISABLED,
         OBSOLETED,
         REFUSED;
